a2d8f29ceafd11ed78eba01cb70c9b8027406ef6
Ashutos sahoo <ashutossahoo3984@gmail.com>
2024-02-10 10:58:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,11 @@ load_dotenv() #load all env variable
 import google.generativeai as genai
 import os
 
-from youtube_transcript_api import YouTubeTranscriptApi
+# from youtube_transcript_api import YouTubeTranscriptApi
+from helper import extract_transcript_details
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY")) 
 
-prompt = """You are youtube video summarizer. You will be taking the transcript text
-and have to summarize the entire video and providing the important summary in points within 250 words"""
-
-def extract_transcript_details(youtube_video_url):
-    try:
-        video_id=youtube_video_url.split("=")[1]
-        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
-        transcript=""
-        for i in transcript_text:
-            transcript += " " + i["text"]
-            
-        return transcript
-    except Exception as e:
-        raise e
 
 def generate_gemini_content(transcript_text,prompt):
     model = genai.GenerativeModel("gemini-pro")
